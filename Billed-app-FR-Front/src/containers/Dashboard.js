24270851163a98b5698e6f0caba429ae5f6c044c
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-06-06 13:39:11 +0200

    fix: issue 4 - visualisation of bills when several lists are open as admin

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -146,8 +146,8 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
+      $(`#open-bill${bill.id}`).off('click').click((e) => this.handleEditTicket(e, bill, bills))
+    })    
 
     return bills
 
